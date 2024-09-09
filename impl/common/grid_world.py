bda076e37ac1dc41ce071ead8f1e8cf39196c553
Wishrem <wishremm@gmail.com>
2024-09-09 19:29:48 +0800

    feat: implemented Policy.get_action_probs function

--- impl/common/grid_world.py
+++ impl/common/grid_world.py
@@ -21,7 +21,7 @@ class Policy:
         self.size = size if isinstance(size, tuple) else (size, size)
         # record the action index whose action value is maximum
         self.action_stars = np.random.randint(
-            0, 5, size=size
+            0, 4, size=size
         )  # 0: up, 1: right, 2: down, 3: left, 4: stay
 
         if eps < 0 or eps > 1:
@@ -29,36 +29,45 @@ class Policy:
 
         self.eps = eps
 
-    def get_action(self, loc: tuple[int, int] | np.ndarray) -> int:
+    @staticmethod
+    def _check_loc(loc: tuple[int, int] | np.ndarray):
         if isinstance(loc, np.ndarray):
             if loc.shape != (2,):
                 raise ValueError(f"loc shape should be (2,), but got {loc.shape}")
             if not np.issubdtype(loc.dtype, np.integer):
                 raise ValueError(f"loc dtype should be np.integer, but got {loc.dtype}")
 
+    def get_action_probs(
+        self, loc: tuple[int, int] | np.ndarray
+    ) -> tuple[list[int], list[float]]:
+        self._check_loc(loc)
+
         num_act = 5
         fact = 1 / num_act * self.eps
-        prob_star = 1 - (num_act - 1) * fact  # range: [1/5, 1]
-        prob = 1 * fact  # range: [0, 1/5]
+        prob_star = 1 - (num_act - 1) * fact
+        prob = 1 * fact
 
         actions = [i for i in range(num_act)]
-        p = []
+        probs = []
 
         idx = tuple(loc)
         action_star = self.action_stars[idx]
         for a in actions:
             if (a == action_star).all():
-                p.append(prob_star)
+                probs.append(prob_star)
             else:
-                p.append(prob)
-        return np.random.choice(actions, 1, p=p)[0]
+                probs.append(prob)
+        return actions, probs
+
+    def get_action(self, loc: tuple[int, int] | np.ndarray) -> int:
+        self._check_loc(loc)
+
+        actions, probs = self.get_action_probs(loc)
+
+        return np.random.choice(actions, 1, p=probs).item()
 
     def improve(self, loc: tuple[int, int] | np.ndarray, q_values: np.ndarray):
-        if isinstance(loc, np.ndarray):
-            if loc.shape != (2,):
-                raise ValueError(f"loc shape should be (2,), but got {loc.shape}")
-            if not np.issubdtype(loc.dtype, np.integer):
-                raise ValueError(f"loc dtype should be np.integer, but got {loc.dtype}")
+        self._check_loc(loc)
 
         if q_values.shape != (5,):
             raise ValueError(
